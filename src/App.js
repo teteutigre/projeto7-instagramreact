6252bbc7383da862d36a2fa886da15321245e2bf
Mateus Tigre <mateus.tigreewallet@gmail.com>
2022-07-15 23:10:56 -0300

    :tada: Commit inicial

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import Topo from "./Topo";
+import Corpo from "./Corpo";
+export default function App() {
+  return <Topo />;
+  <Corpo />;
+}
---
+++ src/Corpo.js
@@ -0,0 +1,10 @@
+export default function Corpo() {
+  return (
+    <div class="story">
+      <div class="imagem">
+        <img src="assets/img/9gag.svg" />
+      </div>
+      <div class="usuario">9gag</div>
+    </div>
+  );
+}
